bbbfd47cfd40cfa0c75c2e50a37a46f6d1d29d32
Tommy Johnson <tommyjohnson1028@gmail.com>
2023-03-09 17:14:49 -0800

    meet the clans page

--- src/pages/meetTheClans.tsx
+++ src/pages/meetTheClans.tsx
@@ -1,39 +1,41 @@
 import Image, { StaticImageData } from "next/image";
 
 import Page from "@/components/Page";
-import Humans from "../../public/clans/humans.svg";
+import Humans from "../../public/clans/login_human_2x.webp";
 import HumansLogo from "../../public/clans/humans_logo_2x.webp";
-import Wildlings from "../../public/clans/wildlings.svg";
+import Wildlings from "../../public/clans/login_wildlings_2x.webp";
 import WildlingsLogo from "../../public/clans/wildlings_logo_2x.webp";
-import Creepers from "../../public/clans/creepers.svg";
+import Creepers from "../../public/clans/login_creepers_2x.webp";
 import CreepersLogo from "../../public/clans/creepers_logo_2x.webp";
-import Cyborgs from "../../public/clans/cyborgs.svg";
+import Cyborgs from "../../public/clans/login_cyborgs_2x.webp";
 import CyborgsLogo from "../../public/clans/synths_logo_2x.webp";
 import { PrimaryButton } from "@/components/buttons/PrimaryButton";
 import { ContainerTitle } from "@/components/typography/ContainerTitle";
 
 const Clan = ({
-  bgColor,
+  className,
   imageSrc,
   logoSrc,
   title,
 }: {
-  bgColor: string;
+  className: string;
   title: string;
   imageSrc: string | StaticImageData;
   logoSrc: string | StaticImageData;
 }) => {
   return (
     <div
-      className={`flex flex-col items-center justify-between w-[250px] h-[505px] bg-[#${bgColor}]`}
+      className={`relative flex flex-col items-center w-[250px] max-w-[250px] h-[505px] ${className}`}
     >
       <div className="mt-6">
         <Image height={87} width={87} src={logoSrc} alt={`Kyogen ${title} Logo`} className="" />
       </div>
-      <div className="mt-3">
+      <div className="mt-3 z-10">
         <ContainerTitle>{title}</ContainerTitle>
       </div>
-      <Image src={imageSrc} alt={`Kyogen ${title}`} className="" />
+      <div className="absolute bottom-0 z-0">
+        <Image src={imageSrc} alt={`Kyogen ${title}`} className="" />
+      </div>
     </div>
   );
 };
@@ -44,25 +46,25 @@ const MeetTheClans = () => {
       <div className="flex flex-col mt-24 items-center">
         <div className="flex flex-row">
           <Clan
-            bgColor="54aeb9"
+            className="bg-[url('../../public/clans/login_bg_human_2x.webp')]"
             imageSrc={Humans}
             logoSrc={HumansLogo}
             title="ANCIENTS"
           />
           <Clan
-            bgColor="ff971d"
+            className="bg-[url('../../public/clans/login_bg_wildlings_2x.webp')]"
             imageSrc={Wildlings}
             logoSrc={WildlingsLogo}
             title="Wildlings"
           />
           <Clan
-            bgColor="e63b47"
+            className="bg-[url('../../public/clans/login_bg_creepers_2x.webp')]"
             imageSrc={Creepers}
             logoSrc={CreepersLogo}
             title="Creepers"
           />
           <Clan
-            bgColor="62c322"
+            className="bg-[url('../../public/clans/login_bg_cyborgs_2x.webp')]"
             imageSrc={Cyborgs}
             logoSrc={CyborgsLogo}
             title="Cyborgs"
