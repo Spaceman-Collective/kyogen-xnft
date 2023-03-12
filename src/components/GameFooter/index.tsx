6bbfd20378e453a94add0a8ea7df41b384bfe1de
Tommy Johnson <tommyjohnson1028@gmail.com>
2023-03-12 16:35:58 -0700

    display unit image

--- src/components/GameFooter/index.tsx
+++ src/components/GameFooter/index.tsx
@@ -1,18 +1,73 @@
-import { selectedUnitAtom } from "@/recoil";
 import { useRecoilValue } from "recoil";
+import { selectedUnitAtom } from "@/recoil";
+import { UnitNames } from "@/types";
+
+import AncientNinja from "../../../public/ancient_ninja.webp";
+import CreeperNinja from "../../../public/creeper_ninja.webp";
+import SynthNinja from "../../../public/synth_ninja.webp";
+import WildingNinja from "../../../public/wildling_ninja.webp";
+import AncientSohei from "../../../public/ancient_sohei.webp";
+import CreeperSohei from "../../../public/creeper_sohei.webp";
+import SynthSohei from "../../../public/synth_sohei.webp";
+import WildingSohei from "../../../public/wildling_sohei.webp";
+import AncientSamurai from "../../../public/ancient_samurai.webp";
+import CreeperSamurai from "../../../public/creeper_samurai.webp";
+import SynthSamurai from "../../../public/synth_samurai.webp";
+import WildingSamurai from "../../../public/wildling_samurai.webp";
+import Image, { StaticImageData } from "next/image";
+
+const UnitNameToImageMap = (name: UnitNames): StaticImageData => {
+  switch (name) {
+    case UnitNames.AncientNinja:
+      return AncientNinja;
+    case UnitNames.AncientSamurai:
+      return AncientSamurai;
+    case UnitNames.AncientSohei:
+      return AncientSohei;
+    case UnitNames.CreeperNinja:
+      return CreeperNinja;
+    case UnitNames.CreeperSamurai:
+      return CreeperSamurai;
+    case UnitNames.CreeperSohei:
+      return CreeperSohei;
+    case UnitNames.WildingNinja:
+      return WildingNinja;
+    case UnitNames.WildingSamurai:
+      return WildingSamurai;
+    case UnitNames.WildingSohei:
+      return WildingSohei;
+    case UnitNames.SynthNinja:
+      return SynthNinja;
+    case UnitNames.SynthSamurai:
+      return SynthSamurai;
+    case UnitNames.SynthSohei:
+      return SynthSohei;
+    default:
+      throw new Error(`Unknown UnitName ${name}`);
+  }
+};
 
 const SelectedUnit = () => {
   const selectedUnit = useRecoilValue(selectedUnitAtom);
   if (!selectedUnit) return null;
 
-  return <div>{selectedUnit.name}</div>;
+  return (
+    <div>
+      <Image
+        src={UnitNameToImageMap(selectedUnit.name)}
+        alt={selectedUnit.name}
+        height={236}
+        width={236}
+      />
+    </div>
+  );
 };
 
 const GameFooter = () => {
   return (
     <div className="flex flex-row grow max-h-[275px]">
       <div className="grow">Mini Map</div>
-      <div className="grow">
+      <div>
         <SelectedUnit />
       </div>
       <div className="grow">Unit Info</div>
