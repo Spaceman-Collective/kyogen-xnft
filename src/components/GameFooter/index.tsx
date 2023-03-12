f079cd9f2438eef2bded13db041e406a71a8afa8
Tommy Johnson <tommyjohnson1028@gmail.com>
2023-03-12 15:57:30 -0700

    outline the game footer

---
+++ src/components/GameFooter/index.tsx
@@ -0,0 +1,12 @@
+const GameFooter = () => {
+  return (
+    <div className="flex flex-row grow max-h-[275px]">
+      <div className="grow">Mini Map</div>
+      <div className="grow">Selected Unit</div>
+      <div className="grow">Unit Info</div>
+      <div className="grow">Game feed</div>
+    </div>
+  );
+};
+
+export default GameFooter;
