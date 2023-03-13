cfacb00d1d4d44f270962976b447b0f36a829b00
Taylor Johnson <nftaylor11@gmail.com>
2023-03-13 14:15:01 -0700

    Gameoverlay (#27)
    * WIP start overlay for player stats
    
    * fix
    
    * overlay icons for score and solarite (#26)
    
    ---------
    
    Co-authored-by: Sam Parhimchik <s.parhim@me.com>

--- src/pages/games/index.tsx
+++ src/pages/games/index.tsx
@@ -1,6 +1,7 @@
 import GameFooter from "@/components/GameFooter";
 import useListenToGameEvents from "@/hooks/useListenToGameEvents";
 import dynamic from "next/dynamic";
+import { GameOverlay } from "../../components/GameOverlay";
 import { useLoadGameState } from "../../hooks/useLoadGameState";
 
 // // must use dynamic imports as `pixi-viewport` expects window object.
@@ -23,8 +24,9 @@ const Game = () => {
     <>
       <div className="h-screen w-screen flex flex-col">
         <p>Game instance</p>
-        <div className="flex flex-1 justify-center items-center">
+        <div className="bg-[#eae6d5] relative flex flex-1 justify-center items-center">
           <GameMap />
+          <GameOverlay />
         </div>
         <GameFooter />
       </div>
