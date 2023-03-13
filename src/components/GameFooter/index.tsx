692ff13b08ea3d0e9cf742ee399e42ff09b48818
Tommy Johnson <32071703+tomjohn1028@users.noreply.github.com>
2023-03-12 20:53:53 -0700

    Game footer clan frame (#23)
    * top border of footer
    
    * vertical border
    
    * full border
    
    * tweak selected unit positioning
    
    * add medal and knot
    
    * add other frame assets
    
    * change frame based on player clan
    
    * add min width to selected unit slot

--- src/components/GameFooter/index.tsx
+++ src/components/GameFooter/index.tsx
@@ -1,6 +1,6 @@
 import { useRecoilValue } from "recoil";
 import { selectedUnitAtom } from "@/recoil";
-import { UnitNames } from "@/types";
+import { Clans, UnitNames } from "@/types";
 
 import AncientNinja from "../../../public/ancient_ninja.webp";
 import CreeperNinja from "../../../public/creeper_ninja.webp";
@@ -14,7 +14,27 @@ import AncientSamurai from "../../../public/ancient_samurai.webp";
 import CreeperSamurai from "../../../public/creeper_samurai.webp";
 import SynthSamurai from "../../../public/synth_samurai.webp";
 import WildingSamurai from "../../../public/wildling_samurai.webp";
+import CreepersBorderImg from "../../../public/clans/creepers/frame_border.svg";
+import CreepersBorderImgY from "../../../public/clans/creepers/frame_border_y.svg";
+import CreepersFrameMedal from "../../../public/clans/creepers/frame_medal.svg";
+import CreepersFrameKnot from "../../../public/clans/creepers/frame_knot.svg";
+
+import AncientsBorderImg from "../../../public/clans/ancients/frame_border_x.svg";
+import AncientsBorderImgY from "../../../public/clans/ancients/frame_border_y.svg";
+import AncientsFrameMedal from "../../../public/clans/ancients/frame_medal.svg";
+import AncientsFrameKnot from "../../../public/clans/ancients/frame_knot.svg";
+
+import SynthsBorderImg from "../../../public/clans/synths/frame_border_x.svg";
+import SynthsBorderImgY from "../../../public/clans/synths/frame_border_y.svg";
+import SynthsFrameMedal from "../../../public/clans/synths/frame_medal.svg";
+import SynthsFrameKnot from "../../../public/clans/synths/frame_knot.svg";
+
+import WildlingsBorderImg from "../../../public/clans/wildlings/frame_border_x.svg";
+import WildlingsBorderImgY from "../../../public/clans/wildlings/frame_border_y.svg";
+import WildlingsFrameMedal from "../../../public/clans/wildlings/frame_medal.svg";
+import WildlingsFrameKnot from "../../../public/clans/wildlings/frame_knot.svg";
 import Image, { StaticImageData } from "next/image";
+import { selectCurrentPlayer } from "@/recoil/selectors";
 
 const UnitNameToImageMap = (name: UnitNames): StaticImageData => {
   switch (name) {
@@ -54,9 +74,10 @@ const HealthBar = ({
   currentHealth: string;
   maxHealth: string;
 }) => {
-  const healthPercentage = parseFloat(currentHealth)/parseFloat(maxHealth) * 100;
+  const healthPercentage =
+    (parseFloat(currentHealth) / parseFloat(maxHealth)) * 100;
   return (
-    <div className="relative bg-[#384269] w-full h-[36px] p-[8px]">
+    <div className="relative bg-[#384269] w-full h-full p-[8px]">
       <div className="relative bg-kyogen-border h-[20px] rounded-[8px] rounded-tr-[0px] z-10">
         <div
           className={`absolute top-0 h-full rounded-[8px] bg-[#7DD75D] border-kyogen-border border-[1px] z-20`}
@@ -70,12 +91,12 @@ const HealthBar = ({
   );
 };
 
-const SelectedUnit = () => {
+const SelectedUnit = ({ className }: { className?: string }) => {
   const selectedUnit = useRecoilValue(selectedUnitAtom);
   if (!selectedUnit) return null;
 
   return (
-    <div>
+    <div className={className}>
       <Image
         src={UnitNameToImageMap(selectedUnit.name)}
         alt={selectedUnit.name}
@@ -90,15 +111,106 @@ const SelectedUnit = () => {
   );
 };
 
+const VerticalBorder = ({ side = "left" }: { side?: "left" | "right" }) => {
+  const player = useRecoilValue(selectCurrentPlayer);
+  if (!player) return null;
+
+  const clan = player.clan;
+
+  let borderAssets = {
+    frameBorder: AncientsBorderImgY,
+    frameMedal: AncientsFrameMedal,
+    frameKnot: AncientsFrameKnot,
+  };
+
+  if (clan === Clans.Creepers) {
+    borderAssets = {
+      frameBorder: CreepersBorderImgY,
+      frameMedal: CreepersFrameMedal,
+      frameKnot: CreepersFrameKnot,
+    };
+  } else if (clan === Clans.Synths) {
+    borderAssets = {
+      frameBorder: SynthsBorderImgY,
+      frameMedal: SynthsFrameMedal,
+      frameKnot: SynthsFrameKnot,
+    };
+  } else if (clan === Clans.Wildings) {
+    borderAssets = {
+      frameBorder: WildlingsBorderImgY,
+      frameMedal: WildlingsFrameMedal,
+      frameKnot: WildlingsFrameKnot,
+    };
+  }
+
+  let borderSideAdjustment = "left-[-9px]";
+  let medalSideAdjustment = "left-[-11px]";
+  let knotSideAdjustment = "left-[-21.5px]";
+  if (side === "right") {
+    borderSideAdjustment = "right-[8px]";
+    medalSideAdjustment = "right-[5px]";
+    knotSideAdjustment = "right-[-5px]";
+  }
+  return (
+    <>
+      <div
+        className={`absolute ${borderSideAdjustment} top-[5px] w-[28px] h-full z-50`}
+        style={{ backgroundImage: `url('${borderAssets.frameBorder.src}')` }}
+      ></div>
+      <Image
+        className={`absolute top-[-14px] z-50 ${medalSideAdjustment}`}
+        src={borderAssets.frameMedal}
+        alt="Frame Medal"
+      />
+      <Image
+        className={`absolute top-0 z-50 ${knotSideAdjustment}`}
+        src={borderAssets.frameKnot}
+        alt="Frame Knot"
+      />
+    </>
+  );
+};
+
+const HorizontalBorder = () => {
+  const player = useRecoilValue(selectCurrentPlayer);
+  if (!player) return null;
+
+  const clan = player.clan;
+  let frameBorder = AncientsBorderImg;
+  if (clan === Clans.Creepers) {
+    frameBorder = CreepersBorderImg;
+  } else if (clan === Clans.Synths) {
+    frameBorder = SynthsBorderImg;
+  } else if (clan === Clans.Wildings) {
+    frameBorder = WildlingsBorderImg;
+  }
+  return (
+    <div
+      className="absolute top-[-9px] w-full h-[18px] bg-repeat-x z-40"
+      style={{ backgroundImage: `url('${frameBorder.src}')` }}
+    ></div>
+  );
+};
+
 const GameFooter = () => {
   return (
-    <div className="flex flex-row grow max-h-[275px]">
+    <div className="relative flex flex-row grow max-h-[275px]">
+      <HorizontalBorder />
+      <VerticalBorder />
       <div className="grow">Mini Map</div>
-      <div>
-        <SelectedUnit />
+      <div className="relative min-w-[245px]">
+        <VerticalBorder />
+        <SelectedUnit className="relative pl-[9px] pt-[2px]" />
       </div>
-      <div className="grow">Unit Info</div>
-      <div className="grow">Game feed</div>
+      <div className="grow relative">
+        <VerticalBorder />
+        Unit Info
+      </div>
+      <div className="grow relative">
+        <VerticalBorder />
+        Game feed
+        <VerticalBorder side="right" />
+      </div>
     </div>
   );
 };
