d867785c0f963a70cca7a9d7c60aef007db0e2cc
Tommy Johnson <tommyjohnson1028@gmail.com>
2023-03-25 15:37:19 -0700

    log structure events

--- src/hooks/useListenToStructureEvents.ts
+++ src/hooks/useListenToStructureEvents.ts
@@ -21,6 +21,8 @@ const useListenToStructureEvents = () => {
       if (!gameState) return;
       const timestamp = Date.now();
 
+      console.log("STRUCTURE EVENT: ", event);
+
       switch (event.name) {
         case "MeteorMined":
           const meteorId = BigInt(event.data.meteor);
